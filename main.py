48e8e2bdf247f58bc5116e9337348990620fe2e3
satviktalchuru <satvik.talchuru@gmail.com>
2026-09-03 17:55:09 -0700

    fixed window counter

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 from abc import ABC, abstractmethod
 
 
@@ -14,4 +15,27 @@ class Algorithm(ABC):
     @abstractmethod
     def allow(self, key: str) -> bool:
         """Return whether request identified by key allowed."""
-    
+
+#fixed window counter algorithm
+class FixedWindowCounter(Algorithm):
+    def __init__(self, limit: int, window: int) -> None:
+        super().__init__(limit, window)
+        self.perkey_state: dict[str, dict[str, float]] = {}
+
+    def allow(self, key: str) -> bool:
+        now = time.time()
+        state = self.perkey_state.get(key)
+
+        if state is None or (now - state["window_start"]) >= self.window:
+            self.perkey_state[key] = {"count": 1, "window_start": now}
+            return True
+
+        if state["count"] >= self.limit:
+            return False
+
+        self.perkey_state[key] = {
+            "count": state["count"] + 1,
+            "window_start": state["window_start"],
+        }
+        return True
+
